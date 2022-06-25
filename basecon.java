d4d19bec3711096ef20536606c792c00cc639023
EfeKN <efe.kaan.fidanci@gmail.com>
2022-06-25 19:18:02 +0300

    decimal conversion to binary added

---
+++ basecon.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class basecon {
+    public static void main(String[] args) {
+
+        Stack<Integer> divisionStack = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter an integer to convert:");
+        System.out.println();
+        int input = sc.nextInt();
+        int inputC = input; //copy of the input
+        
+        // converter algorithm
+        while(inputC!=0) {
+            divisionStack.push(inputC%2);
+            inputC /= 2; 
+        }
+
+        String output = "";
+        while(!divisionStack.empty()) {
+            output += Integer.toString(divisionStack.pop());
+        }
+        System.out.println("This integer equals to " + output + ".\n");
+
+        sc.close();
+    }
+}
