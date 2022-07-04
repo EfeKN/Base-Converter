ba35c00f652d9a6177e7a45198e417cc0f11a3dc
EfeKN <efe.kaan.fidanci@gmail.com>
2022-07-04 16:50:18 +0300

    added calculator class & added a simple GUI

--- basecon.java
+++ basecon.java
@@ -1,11 +1,70 @@
 import java.util.Scanner;
 import java.util.Stack;
 
+import javax.swing.Box;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
+@SuppressWarnings("unusued")
 public class basecon {
     public static void main(String[] args) {
 
+        calculator cal = new calculator();
         Stack<Integer> divisionStack = new Stack<>();
         Scanner sc = new Scanner(System.in);
+
+        //
+        for(int i=100; i>0; i--) 
+        {
+            int rand = cal.randomBinaryNumber(2);
+            System.out.println("Binary: " + rand);
+            System.out.println("Decimal Value: " + cal.convertDecimal(rand, 2));
+        }
+        //
+
+        //Setting up the window
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("Base Converter");
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        //User interactive area
+        JTextField tf = new JTextField("0                         ");
+        JComboBox<Integer> cb = new JComboBox<>();
+        cb.addItem(2);
+        cb.addItem(3);
+        cb.addItem(4);
+        cb.addItem(5);
+        cb.addItem(6);
+        cb.addItem(7);
+        cb.addItem(8);
+        cb.addItem(9);
+        cb.addItem(10);
+        JLabel outp = new JLabel("0         ");
+        //Add components to the frame
+        panel.add(tf,c);
+        panel.add(Box.createHorizontalStrut(20),c); // Space between components
+        panel.add(cb,c);
+        panel.add(Box.createHorizontalStrut(20),c); // Space between components
+        panel.add(outp,c);
+        frame.add(panel);
+        
+
+        frame.setVisible(true);
+        
+        
+        
+        
+        
+        
         
         System.out.print("Enter an integer to convert: ");
         int input = sc.nextInt();
@@ -25,4 +84,5 @@ public class basecon {
 
         sc.close();
     }
+
 }
---
+++ calculator.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+
+public class calculator {
+
+    /* 
+    
+    http://www.cs.trincoll.edu/~ram/cpsc110/inclass/conversions.html#:~:text=Algorithm%20to%20Convert%20From%20Any,is%20decimal%20so%20b%20%3D%2010.
+
+    */
+    
+    int convertDecimal(int value, int oBase) {
+        int length = (int) (Math.log10(value) + 1);
+        int converted = 0;
+        String number = Integer.toString(value);
+        while(!number.isEmpty())
+        {
+            converted += Character.getNumericValue(number.charAt(0))*Math.pow(oBase, length-1);
+            number = number.substring(1);
+            length += -1;
+        }
+        return converted;
+    }
+
+    int convertBase(int tBase) {
+        return 0;
+    }
+
+    //from https://www.geeksforgeeks.org/find-first-last-digits-number/
+    int firstDigit(int n) {
+        // Remove last digit from number
+        // till only one digit is left
+        while (n >= 10) 
+        n /= 10;
+  
+        // return the first digit
+        return n;
+    }
+
+    /* This part needs improvement */
+
+        /* This cannot solve the issue with zeros */
+
+            /* As a temporary solution random cannot produce zeros */
+
+    int randomBinaryNumber(int digit) {
+        return randomBaseNumber(digit, 2);
+    }
+
+    int randomBaseNumber(int digit, int base) {
+        Random r = new Random();
+        int gNumber = 0;
+        for(int i=0; i<digit; i++)
+        {
+            int add = 0;
+            while(add==0)
+            {
+                add = r.nextInt(base);
+            }
+            gNumber += add*Math.pow(10, i);
+        }
+        return gNumber; 
+    }
+}
